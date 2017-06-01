4cdf9b6d238ceb18723ce6e0d3af6173e5b1b952
Liyu Gong <liyugong@Liyus-MacBook-Pro.local>
2017-05-31 22:12:02 -0400

    ss

--- hw3/hw3_LowestCommonAncestorofaBinarySearchTree.java
+++ hw3/hw3_LowestCommonAncestorofaBinarySearchTree.java
@@ -1,6 +1,6 @@
 package leet_hw1;
 /**
- * Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+ * #235 Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
  * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes v and w 
  * as the lowest node in T that has both v and w as descendants (where we allow a node to be a descendant of itself).”
  *
@@ -16,61 +16,33 @@ package leet_hw1;
  * @author liyugong
  */
 
- 
-
 public class hw3_LowestCommonAncestorofaBinarySearchTree {
-	 public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-		while(isAncester(root, p) && isAncester(root,q)){
-			if(isAncester(root.left, p) && isAncester(root.left,q)){
-				root = root.left;
-			}
-			else if(isAncester(root.right, p) && isAncester(root.right,q)){
-				root = root.right;
-			}
-			else{
-				return root;
-			}
-		}
+//	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//		while (isAncester(root, p) && isAncester(root, q)) {
+//			if (isAncester(root.left, p) && isAncester(root.left, q)) {
+//				root = root.left;
+//			} else if (isAncester(root.right, p) && isAncester(root.right, q)) {
+//				root = root.right;
+//			} else {
+//				return root;
+//			}
+//		}
+//		return root;
+//
+//	}
+//
+//	public boolean isAncester(TreeNode root, TreeNode p) {
+//		if (root == null) {
+//			return false;
+//		}
+//		if (root == p)
+//			return true;
+//		return isAncester(root.left, p) || isAncester(root.right, p);
+//	}
+
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		while ((root.val - p.val) * (root.val - q.val) > 0)
+			root = p.val < root.val ? root.left : root.right;
 		return root;
-		
-	 }
-	 public boolean isAncester(TreeNode root, TreeNode p){
-		 if(root == null){
-			 return false;	
-		 }
-		 if(root == p)
-			 return true;
-		 return isAncester(root.left, p) || isAncester(root.right,p);
-	 }
-	 public static void main(String[] args){
-			hw3_LowestCommonAncestorofaBinarySearchTree a = new hw3_LowestCommonAncestorofaBinarySearchTree();
-			TreeNode b = new TreeNode(5);
-			TreeNode c = new TreeNode(4);
-			TreeNode d = new TreeNode(8);
-			TreeNode e = new TreeNode(11);
-			TreeNode f = new TreeNode(13);
-			TreeNode g = new TreeNode(4);
-			TreeNode h = new TreeNode(7);
-			TreeNode i = new TreeNode(2);
-			TreeNode j = new TreeNode(1);
-			b.left = c;
-			b.right = d;
-			c.left = e;
-			c.right = null;
-			d.left = f;
-			d.right = g;
-			e.left = h;
-			e.right = i;
-			f.left = null;
-			f.right =null;
-			g.left = null;
-			g.right = j;
-			h.left = null;
-			h.right =null;
-			i.left = null;
-			i.right =null;
-			j.left = null;
-			j.right =null;
-			System.out.print(a.lowestCommonAncestor(b, h, f).val);
-		}
+	}
 }
